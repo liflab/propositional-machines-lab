8cea6a53ceeef60a1f395ab4ee4a235e0877fe6a
Sylvain Hallé <shalle@acm.org>
2020-06-01 16:23:32 -0400

    First commit of labpal-project

---
+++ Source/src/mylab/MyLaboratory.java
@@ -0,0 +1,18 @@
+package mylab;
+
+import ca.uqac.lif.labpal.Laboratory;
+
+public class MyLaboratory extends Laboratory
+{
+	@Override
+	public void setup()
+	{
+		// Write your setup code here
+	}
+	
+	public static void main(String[] args)
+	{
+		// Nothing else to do here
+		MyLaboratory.initialize(args, MyLaboratory.class);
+	}
+}
