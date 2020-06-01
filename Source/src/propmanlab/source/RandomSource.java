1ac88f9cb4e957414fc72c35611764c27d4c9019
Sylvain Hallé <shalle@acm.org>
2020-06-01 16:54:55 -0400

    Updated GPL comments

--- Source/src/propmanlab/source/RandomSource.java
+++ Source/src/propmanlab/source/RandomSource.java
@@ -1,6 +1,6 @@
 /*
-    A benchmark for data lineage in BeepBeep 3
-    Copyright (C) 2017-2020 Laboratoire d'informatique formelle
+    A benchmark for propositional machines in BeepBeep 3
+    Copyright (C) 2020 Laboratoire d'informatique formelle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published
